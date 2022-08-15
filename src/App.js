bc1e0b3fc781e0eac04ca776c9b5a71eea2e42fb
luisparf <luispauloaraujoafonso@gmail.com>
2022-08-15 12:17:31 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { DonutLarge } from '@material-ui/icons';
+import { Chat } from '@mui/icons-material';
+import { MoreVert } from '@material-ui/icons';
+import { Search } from '@mui/icons-material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () =>{
+  return(
+    <div className='app-window'>
+        <div className='sidebar'>
+
+              <header>
+                <img className='header--avatar' src='https://www.w3schools.com/howto/img_avatar.png' alt=''></img>
+                <div className='headder--buttons'>
+                  <div className='header--btn'>
+                      <DonutLarge style={{color: '#919191'}} />
+                  </div>
+                  <div className='header--btn'>
+                      < Chat style={{color: '#919191'}} />
+                  </div>
+                  <div className='header--btn'>
+                      <MoreVert style={{color: '#919191'}} />
+                  </div>
+
+                </div>
+              </header>
+
+              <div className='search'>
+                <div className='search--input'>
+                     < Search fontSize='small' style={{color: '#919191'}} />
+                     <input type='search' placeholder='Procurar ou começar uma nova conversa'/> 
+
+                </div>
+              </div>
+
+              <div className='chatlist'>
+                ...
+              </div>
+        </div>
+        <div className='contentarea'>
+      </div>
     </div>
   );
 }
-
-export default App;
